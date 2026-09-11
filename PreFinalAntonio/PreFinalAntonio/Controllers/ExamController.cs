faff298e49b95ada4009c7ffc8046851e1d32f89
ANTONIOMARCKEVIN <klyneskevin0818@gmail.com>
2026-09-11 21:13:06 +0800

    Add question 12: Include navigation benefit

--- PreFinalAntonio/PreFinalAntonio/Controllers/ExamController.cs
+++ PreFinalAntonio/PreFinalAntonio/Controllers/ExamController.cs
@@ -76,6 +76,12 @@ namespace PreFinalAntonio.Controllers
                     QuestionText = "Why might a ViewModel be used when displaying Student and Section information?",
                     Options = new() { "A. To replace the database", "B. To combine or shape the data specifically needed by the view", "C. To automatically create database tables", "D. To prevent controllers from using LINQ" },
                     CorrectAnswer = "B. To combine or shape the data specifically needed by the view"
+                },
+                new ExamItem {
+                    Number = 12,
+                    QuestionText = "Consider this query: var students = _context.Students.Include(s => s.Section).ToList(); What is the main benefit of Include(s => s.Section)?",
+                    Options = new() { "A. It loads the related Section navigation property", "B. It creates a Section object manually", "C. It removes the foreign key", "D. It prevents the query from accessing the database" },
+                    CorrectAnswer = "A. It loads the related Section navigation property"
                 }
 
             };
